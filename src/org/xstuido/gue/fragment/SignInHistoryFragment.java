21196bd53cbae6a74234abbbe3888dd4197fb7f8
limkuan <limkuan@limkuan-PC.lan>
2014-07-22 01:27:56 +0800

    2014年7月22日01:27:52

--- src/org/xstuido/gue/fragment/SignInHistoryFragment.java
+++ src/org/xstuido/gue/fragment/SignInHistoryFragment.java
@@ -9,32 +9,20 @@ import org.xstuido.gue.activity.BaseApplication;
 import org.xstuido.gue.adapter.SignHistoryAdapter;
 import org.xstuido.gue.db.GetUpEarlyDB;
 import org.xstuido.gue.util.Event;
-import org.xstuido.gue.util.Tool;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class SignInHistoryFragment extends Fragment {
 
-	private TextView mSignTextView;
 	private ListView mHistoryListView;
 	private GetUpEarlyDB mDB;
 
 	private boolean isInit;
-
-	private OnClickListener mSignClickListener = new OnClickListener() {
-
-		@Override
-		public void onClick(View v) {
-			Tool.showToast("Click");
-		}
-	};
 
 	public SignInHistoryFragment() {
 		mDB = new GetUpEarlyDB(BaseApplication.getContext());
@@ -44,7 +32,6 @@ public class SignInHistoryFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View main = inflater.inflate(R.layout.fragment_sign_history, null);
-		mSignTextView = (TextView) main.findViewById(R.id.tv_significant);
 		mHistoryListView = (ListView) main.findViewById(R.id.lv_history);
 
 		if (!isInit) {
@@ -65,7 +52,6 @@ public class SignInHistoryFragment extends Fragment {
 	}
 
 	private void initView() {
-		mSignTextView.setOnClickListener(mSignClickListener);
 	}
 
 	@Override
